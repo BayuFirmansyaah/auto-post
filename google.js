9fb4c28a143c2fe04fa4485b56db9389557725c7
zulfarizqi <bayufirmansyah2800>
2021-04-07 09:50:12 +0700

    membuat sort json berdasarkan username akun asc

--- google.js
+++ google.js
@@ -7,12 +7,36 @@ let data = {
     path: null
 };
 
+
+// function sort json
+function sortByProperty(objArray, prop, direction) {
+    if (arguments.length < 2) throw new Error("ARRAY, AND OBJECT PROPERTY MINIMUM ARGUMENTS, OPTIONAL DIRECTION");
+    if (!Array.isArray(objArray)) throw new Error("FIRST ARGUMENT NOT AN ARRAY");
+    const clone = objArray.slice(0);
+    const direct = arguments.length > 2 ? arguments[2] : 1; //Default to ascending
+    const propPath = (prop.constructor === Array) ? prop : prop.split(".");
+    clone.sort(function (a, b) {
+        for (let p in propPath) {
+            if (a[propPath[p]] && b[propPath[p]]) {
+                a = a[propPath[p]];
+                b = b[propPath[p]];
+            }
+        }
+        // convert numeric strings to integers
+        a = a.match(/^\d+$/) ? +a : a;
+        b = b.match(/^\d+$/) ? +b : b;
+        return ((a < b) ? -1 * direct : ((a > b) ? 1 * direct : 0));
+    });
+    return clone;
+}
+
 // mendapatkan data akun facebook
 $.ajax({
     url: 'http://localhost:3000/get/account/' + sessionStorage.getItem('id_user'),
     success: (Account) => {
         let akun = Account.Search;
-        data.akun = akun;
+        akun = sortByProperty(akun, "akun.username");
+        console.log(akun);
         let row = "";
         let i = 1;
         akun.forEach(data => {
