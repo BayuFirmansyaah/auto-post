5b7115ac9492e62d025421d3762be223dd3ad7fd
zulfarizqi <bayufirmansyah2800>
2021-04-26 13:41:00 +0700

    .

--- directory.js
+++ directory.js
@@ -1,14 +1,17 @@
 
-        $(document).ready(function () {
 
-        });
-        if (localStorage.getItem('ls') === "true") {
-            document.querySelector('.sidebar').classList.remove('sidebar-none')
-            document.querySelector('.sidebar').classList.add('sidebar-block')
-            document.querySelector('.content').classList.remove('table-none')
-            document.querySelector('.table').classList.remove('table-full')
-            document.querySelector('.row-table').classList.remove('.row-table-none')
-            $('.sidebar').html(` <h6 class="text-heading text-primary">DATA ACCOUNTS</h6>
+
+
+$(document).ready(function () {
+
+});
+if (localStorage.getItem('ls') === "true") {
+    // document.querySelector('.table').classList.remove('table-full')
+    // document.querySelector('.row-table').classList.remove('.row-table-none')
+    document.querySelector('.sidebar').classList.remove('sidebar-none')
+    document.querySelector('.sidebar').classList.add('sidebar-block')
+    document.querySelector('.content').classList.remove('table-none')
+    $('.sidebar').html(` <h6 class="text-heading text-primary">DATA ACCOUNTS</h6>
     <a href="/google.html">
         <p class="sub-heading "><i class="fab fa-facebook"></i> Facebook Accounts</p>
     </a>
@@ -24,13 +27,13 @@
             <p class="sub-heading "><i class="fas fa-folder-tree"></i> File Manager</p>
         </a>`)
 
-        } else if (localStorage.getItem('ls') === "false") {
-            document.querySelector('.sidebar').classList.add('sidebar-none')
-            document.querySelector('.sidebar').classList.remove('sidebar-block')
-            document.querySelector('.content').classList.add('table-none')
-            document.querySelector('table').classList.add('table-full')
-            document.querySelector('.row-table').classList.add('.row-table-none')
-            $('.sidebar').html(`  <a href="/google.html">
+} else if (localStorage.getItem('ls') === "false") {
+    document.querySelector('.sidebar').classList.add('sidebar-none')
+    document.querySelector('.sidebar').classList.remove('sidebar-block')
+    document.querySelector('.content').classList.add('table-none')
+    document.querySelector('table').classList.add('table-full')
+    document.querySelector('.row-table').classList.add('.row-table-none')
+    $('.sidebar').html(`  <a href="/google.html">
     <p class="sub-heading "><i class="fab fa-facebook"></i></p>
 </a>
 <a href="/indexing.html">
@@ -44,26 +47,32 @@
         </a>`)
 
 
-        }
+}
 
-        let count = 0;
-        let content = document.querySelector('.content');
-        content.addEventListener('contextmenu', function (ev) {
-            ev.preventDefault();
-            let x = ev.clientX;
+$('.content').mousedown(function (event) {
+    switch (event.which) {
+        case 1:
+            document.querySelector('.side-menu-click').style.display = "none"
+            break;
+        case 2:
+            alert('Middle Mouse button pressed.');
+            break;
+        case 3:
+            let x = event.clientX;
             x -= 260;
-            let y = ev.clientY;
+            let y = event.clientY;
             y -= 200;
-            if (count == 0) {
-                document.querySelector('.side-menu-click').style.marginLeft = x + "px"
-                document.querySelector('.side-menu-click').style.marginTop = y + "px"
-                document.querySelector('.side-menu-click').style.display = "block"
-                count+=1;
-            } else {
-                document.querySelector('.side-menu-click').style.display = "none"
-                count-=1;
-            }
+            document.querySelector('.side-menu-click').style.marginLeft = x + "px"
+            document.querySelector('.side-menu-click').style.marginTop = y + "px"
+            document.querySelector('.side-menu-click').style.display = "block"
+            break;
+        default:
+            alert('You have a strange Mouse!');
+    }
+});
 
-            console.log(x, y);
-            return false;
-        }, false);
+let content = document.querySelector('.content');
+content.addEventListener('contextmenu', function (ev) {
+    ev.preventDefault();
+    return false;
+}, false);
